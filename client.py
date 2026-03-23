30f4dfa74eb6dc498167f7fb9d7126c9b0b033f1
lynylyn <evelyn.starling@education.nsw.gov.au>
2026-03-23 19:47:30 +1100

    Login & requestnoauth
    Basic client requests

--- client.py
+++ client.py
@@ -2,21 +2,27 @@ import requests
 import configparser
 import hashlib
 import xml.etree.ElementTree as ET
+import webbrowser
 base_url = "https://ws.audioscrobbler.com/2.0"
 config = configparser.ConfigParser()
 api_key = config["keys"]["key"]
 api_secret = config["keys"]["secret"]
 loggedin = False
+session_key = None
 
 def requestauth(method, params):
-    pass
+    fullparams = params.copy()
+    fullparams["sk"] = session_key
+    return requestnoauth(method, fullparams)
 
 def requestnoauth(method, params):
     fullparams = params.copy()
     fullparams["api_key"] = api_key
     fullparams["method"] = method
     fullparams["api_sig"] = makesignature(fullparams)
-    r = requests.get(base_url, params=fullparams)
+    r = requests.get(base_url,
+                     params=fullparams,
+                     headers={"user-agent": "Evelyn Starling's Project <evechrsta@gmail.com>"})
     tree = ET.parse(r.text)
     return tree.getroot()
 
@@ -36,7 +42,15 @@ def md5(text):
     return h.hexdigest()
 
 def login():
-    request("auth.getToken")
+    response = requestnoauth("auth.getToken", {})
+    #lfm status ok ahhh error handling aahhh
+    token = response.find("token").text
+    webbrowser.open (f"http://www.last.fm/api/auth/?api_key={api_key}&token={token}")
+    input("Please press enter to continue.")
+    response = requestnoauth("auth.getSession", {"token": token})
+    global session_key
+    session_key = response.find("session/key").text
+
 
 def trackGetInfo(song_name, artist_name):
     setup()
